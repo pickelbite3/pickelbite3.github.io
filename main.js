7a20cb028bb4e52c07fa1c10c07aeb14cbc872f1
pickelbyte4 <yodelingpickle79@gmail.com>
2024-06-17 10:59:38 -0700

    test random game

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ var sortAscending = false;
 const sortbtn = document.getElementById('sortToggle');
 const gamesContainer = document.getElementById("games-container");
 const originalOrder = Array.from(gamesContainer.getElementsByClassName("game"));
+const gamesCount = document.getElementById("count");
+
+
+console.log(originalOrder);
+
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
 
 function filterGames() {
     const searchInput = document.querySelector('.search-input');
@@ -40,4 +49,30 @@ function toggleSort() {
 function closeAlert() {
     const alertBox = document.querySelector(".alert");
     alertBox.style.display = "none";
+}
+
+function pickRandom() {
+    rnum = getRndInteger(0, parseInt(gamesCount.textContent))
+    rnum--;
+
+    // get the numbered game in the list, generate link for that game, go to it by setting rgame to it
+
+    game = originalOrder[rnum];
+    console.log(game)
+
+    var htmlString = game.innerHTML;
+
+    // Create a temporary DOM element
+    var tempElement = document.createElement('div');
+    
+    // Set the innerHTML of the temporary element to the HTML string
+    tempElement.innerHTML = htmlString;
+    
+    // Extract the href attribute from the first 'a' tag within the temporary element
+    var link = tempElement.querySelector('a').getAttribute('href');
+    
+    // Output the link
+    console.log(link); // Outputs: /games/roadblocks/index.html
+
+    window.location = link;
 }
